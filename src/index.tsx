5ccca862fe3124aa293071a6afe2e7f0b3158094
Emirhan PALA <emirhanpala1661@gmail.com>
2024-04-14 00:58:42 +0300

    Fixing the error occurring while using flex.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { InSimFlags } from 'node-insim/packets';
 import React, { StrictMode } from 'react';
-import type { CreateRootOptions } from "react-node-insim";
 import {
   ConnectionsProvider,
   createRoot,
@@ -10,6 +9,7 @@ import { Provider } from 'react-redux';
 
 import { App } from "./App";
 import store from './store';
+import type { CreateRootOptions } from './types';
 
 const flags = InSimFlags.ISF_OBH | InSimFlags.ISF_MCI | InSimFlags.ISF_CON | InSimFlags.ISF_MSO_COLS | InSimFlags.ISF_NLP | InSimFlags.ISF_AXM_EDIT | InSimFlags.ISF_AXM_LOAD;
 
--- src/pages/base/Dashboard.tsx
+++ src/pages/base/Dashboard.tsx
@@ -1,44 +1,36 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-empty-interface */
-import React, { useEffect, useState } from 'react';
-import { Button, Flex, Grid, GridButton, HStack, TextBox, useConnections, VStack } from 'react-node-insim';
+import React from 'react';
+import { Button, Flex } from 'react-node-insim';
 import { useAppSelector } from 'src/hooks/hooks';
+import { translate } from 'src/i18n/translate';
 import { helper } from 'src/utils/helper';
 
-interface IDashboardProps { };
-
 const translatePath = "apps|dashboard";
-export const Dashboard = (props: IDashboardProps) => {
-    const connections = useConnections();
+export const Dashboard = () => {
     const mciState = useAppSelector(s => s.mci);
     return (
-        <>
-            {connections?.size > 0 && connections?.map((m, k) => (
-                <React.Fragment key={m.UCID}>
-                    <Flex
-                        top={84}
-                        left={169}
-                        width={27}
-                        alignItems="center"
-                        wrap="wrap"
-                        background="dark"
-                        backgroundColor="light"
-                        UCID={m.UCID}
-                        padding={1}
-                    >
-                        <Button width={25} height={4} align="left">^1React Cruise InSim</Button>
-                        <Button width={25} height={4} align="left">^7Money: ^3TL 25943</Button>
-                        <Button width={25} height={4} align="left">^7Bonus: ^3%19 (1)</Button>
-                        <Button width={25} height={4} align="left">^7Distance: ^315106.7 ^7Km</Button>
-                        <Button width={25} height={4} align="left">^7Car: ^3XFG</Button>
-                        <Button width={25} height={4} align="left">^7I.R: ^30 ^7A.M: ^30</Button>
-                        <Button width={25} height={4} align="left">^7Speed Limit: ^1{mciState.speedLimit} ^7Km/h</Button>
-                        <Button width={25} height={4} align="left" >^7Way: ^3{`${mciState.wayName}`}</Button>
-                        <Button width={25} height={4} align="left">{mciState.freePit ? "^6You can enter the pit now!" : "^1Entering the pit costs ^7$^1500"}</Button>
-                    </Flex>
-                    <Button width={25} height={6} align="left" top={194} left={25} UCID={m.UCID} background="dark">^2Speed: {(mciState.userSpeed > mciState.speedLimit) ? "^1" : "^7"}{helper.formatNumber(mciState.userSpeed, 0)} km/h</Button>
-                </React.Fragment>
-            ))}
-        </>
+        <React.Fragment key={"Flex-1"}>
+            <Flex
+                top={84}
+                left={169}
+                width={27}
+                alignItems="center"
+                wrap="wrap"
+                background="dark"
+                backgroundColor="light"
+                UCID={255}
+                padding={1}
+            >
+                <Button width={25} height={4} align="left">^1React Cruise InSim</Button>
+                <Button width={25} height={4} align="left">^7{translate(translatePath, "Money")}: ^3TL 25943</Button>
+                <Button width={25} height={4} align="left">^7{translate(translatePath, "Bonus")}: ^3%19 (1)</Button>
+                <Button width={25} height={4} align="left">^7{translate(translatePath, "Distance")}: ^315106.7 ^7Km</Button>
+                <Button width={25} height={4} align="left">^7{translate(translatePath, "Car")}: ^3XFG</Button>
+                <Button width={25} height={4} align="left">^7{translate(translatePath, "I.R")}: ^30 ^7{translate(translatePath, "A.M")}: ^30</Button>
+                <Button width={25} height={4} align="left">^7{translate(translatePath, "Speed Limit")}: ^1{mciState.speedLimit} ^7Km/h</Button>
+                <Button width={25} height={4} align="left">^7{translate(translatePath, "Way")}: ^3{`${mciState.wayName}`}</Button>
+                <Button width={25} height={4} align="left">{mciState.freePit ? translate(translatePath, "^6You can enter the pit now!") : translate(translatePath, "^1Entering the pit costs ^7$^1500")}</Button>
+            </Flex>
+            <Button width={25} height={6} align="left" top={194} left={25} UCID={255} background="dark">^2Speed: {(mciState.userSpeed > mciState.speedLimit) ? "^1" : "^7"}{helper.formatNumber(mciState.userSpeed, 0)} km/h</Button>
+        </React.Fragment>
     );
 };
